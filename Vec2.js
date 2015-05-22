49904fa5ef4d9630ee34927690458aabb61c4b48
Ryan Hofer <r.h.hofer@gmail.com>
2015-05-22 12:37:48 -0500

    Import Vec2 class

---
+++ Vec2.js
@@ -0,0 +1,26 @@
+'use strict';
+
+function Vec2(x, y) {
+  this.x = x;
+  this.y = y;
+}
+
+Vec2.prototype.setv = function(v) {
+  this.x = v.x;
+  this.y = v.y;
+}
+
+Vec2.prototype.addv = function(v) {
+  this.x += v.x;
+  this.y += v.y;
+}
+
+Vec2.prototype.set = function(x, y) {
+  this.x = x;
+  this.y = y;
+}
+
+Vec2.prototype.add = function(x, y) {
+  this.x += x;
+  this.y += y;
+}
