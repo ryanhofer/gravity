64769c6e4f674ba05429cfbfcc2f9f49f33197e9
Ryan Hofer <r.h.hofer@gmail.com>
2015-05-22 21:08:18 -0500

    Group Vec2 methods by function rather than params

--- Vec2.js
+++ Vec2.js
@@ -10,14 +10,14 @@ Vec2.prototype.setv = function (v) {
   this.y = v.y;
 };
 
-Vec2.prototype.addv = function (v) {
-  this.x += v.x;
-  this.y += v.y;
-};
-
 Vec2.prototype.set = function (x, y) {
   this.x = x;
   this.y = y;
+};
+
+Vec2.prototype.addv = function (v) {
+  this.x += v.x;
+  this.y += v.y;
 };
 
 Vec2.prototype.add = function (x, y) {
